b3018974b5ecb133bbf4dd87ba8bde6418b3b2d6
NehaRidhma <94739547+NehaRidhma@users.noreply.github.com>
2022-02-09 14:46:08 +0530

    Add files via upload

---
+++ scratch_canvas (15).py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+years = [2017,2018,2019,2020]
+population = [19.756,19.979,20.185,20.411]
+
+plt.bar(years,population)
+plt.xlabel('year')
+plt.ylabel('population in lakhs')
+plt.title('population raph of mumbai')
+plt.show()
